ed2cbfaa527f66176d8c48d08253bd5c6e3e1567
Guruen <Guruen@geez.dk>
2022-01-31 12:19:28 +0100

    first commit

--- src/app/modules/drivhus/dto/currentTemperature.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface CurrentTemperatureDto {
-  temperature_inside_1: number;
-  temperature_inside_2: number;
-  temperature_outside: number;
-  temperature_water: number;
-}
--- src/app/modules/drivhus/dto/wifi.dto.ts
+++
@@ -1,3 +0,0 @@
-export interface wifiDto {
-  rssi: number;
-}
